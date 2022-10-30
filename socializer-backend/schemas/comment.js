cb246202b11c4449f5766f8dba05203680e3895f
Calinpet <92047573+Calinpet@users.noreply.github.com>
2022-10-30 14:37:14 -0400

    created comment  schema

---
+++ socializer-backend/schemas/comment.js
@@ -0,0 +1,18 @@
+// create comments schema
+export default {
+  name: 'comment',
+  title: 'Comment',
+  type: 'document',
+  fields: [
+    {
+      name: 'postedBY',
+      title: 'Posted By',
+      type: 'postedBy'
+    },
+    {
+      name: 'comment',
+      title: 'Comment',
+      type: 'string',
+    }
+  ]
+}
