e523327882939fb588afa29aeabfa1fe52f3a787
Calinpet <92047573+Calinpet@users.noreply.github.com>
2022-10-30 18:29:45 -0400

    created the export function getServerSideProps to make req to backend

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
 import type { NextPage } from "next"
+import axios from "axios";
+
 
 const Home: NextPage = () => {
   return (
@@ -7,5 +9,17 @@ const Home: NextPage = () => {
     </h1>
   )
 }
+
+// fetching data 
+// used the docs https://nextjs.org/docs/basic-features/data-fetching/get-server-side-props
+// If you export a function called getServerSideProps (Server-Side Rendering) from a page, Next.js will pre-render this page on each request using the data returned by getServerSideProps.
+// only runs on server-side and never runs on the browser.
+// You should use getServerSideProps only if you need to render a page whose data must be fetched at request time. 
+
+export const getServerSideProps = async () => {
+   // will be passed to the page component as props
+  //  making a get request to our own backend
+   const response = await axios.get();
+  }
 
 export default Home
