2d2b9ee20a144cbbf24c36a5b5afecdce25dfafa
Calinpet <92047573+Calinpet@users.noreply.github.com>
2022-10-30 18:16:41 -0400

    created footer

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -7,9 +7,13 @@ import { topics } from '../utils/constants'
 const Discover = () => {
 
   const router = useRouter();
+  // extract the topic from the router by using object destructuring
   const { topic } = router.query;
 
+  // create active topic style
   const activeTopicStyle = 'xl:border-2 hover:bg-primary xl:border-[#F51997] px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-[#F51997]';
+
+  // create topic style
   const topicStyle = 'xl:border-2 hover:bg-primary xl:border-gray-300 px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-black';
 
   return (
@@ -21,6 +25,7 @@ const Discover = () => {
         {/* creating a dynamic block of code to point to  all topics */}
         {topics?.map((item) => (
           <Link href={`/?topic=${item.name}`} key={item.name}>
+            {/* if topic is equal to item.nmae than render */}
             <div className={topic === item.name ? activeTopicStyle : topicStyle}>
               {/* render the icons */}
               <span className='font-bold text-2xl xl:text-md '>
