a0722e3020c4dc36101397116e00ea486dfd2262
Calinpet <92047573+Calinpet@users.noreply.github.com>
2022-10-30 20:41:58 -0400

    implemened function to add profilwe image

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -1,6 +1,14 @@
-import React from 'react'
-import { Video } from '../types'; 
+import React, { useEffect, useRef, useState } from 'react';
+ 
 import { NextPage } from 'next';
+import Image from 'next/image';
+import Link from 'next/link'
+import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi';
+import { BsFillPlayFill, BsFillPauseFill } from 'react-icons/bs';
+import { GoVerified } from 'react-icons/go';
+import { BsPlay } from 'react-icons/bs';
+
+import { Video } from '../types';
 
 interface IProps {
   post: Video;
@@ -10,7 +18,26 @@ const VideoCard: NextPage <IProps> = ({ post }) => {
 
   console.log(post.caption)
   return (
-    <div>VideoCard</div>
+    <div className='flex flex-col border-b-2 border-gray-200 pb-6'>
+      <div>
+        <div className=' flex gap-3 p-2 cursor-pointer font-semibold rounded'>
+          <div className='md:w-16 md:h-16 w-10 h-10'>
+            <Link href="/">
+              <>
+                <Image 
+                  width={62}
+                  height={62}
+                  className="rounded-full"
+                  src={post.postedBy.image}
+                  alt="profile photo"
+                  layout="responsive"
+                />
+              </>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
