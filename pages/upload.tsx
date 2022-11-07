368b38d6c1a5b135aca5615e6701785fcf7638a2
Calinpet <92047573+Calinpet@users.noreply.github.com>
2022-11-07 18:36:51 -0500

    added uploading functionality

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -8,9 +8,33 @@ import axios from 'axios';
 import useAuthStore from '../store/authStore';
 import { client } from '../utils/client';
 import { topics } from '../utils/constants';
+import { GiMp40 } from 'react-icons/gi';
 
 const Upload = () => {
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
+  const [videoAsset, setVideoAsset] = useState<SanityAssetDocument | undefined>();
+  const [wrongFileType, setWrongFileType] = useState(false);
+
+  // create the callback function for uploadVideo
+  const uploadVideo = async (e: any) => {
+    const selectedFile = e.target.files[0];
+    const fileTypes = ['video/mp4', 'video/webm', 'video/ogg'];
+
+    if(fileTypes.includes(selectedFile.type)) {
+      client.assets.upload('file', selectedFile, {
+        contentType: selectedFile.type,
+        filename: selectedFile.name
+      })
+      //.then method if the upload is succesfull 
+      .then((data) => {
+        setVideoAsset(data);
+        setIsLoading(false); 
+      })  
+    } else {
+      setIsLoading(false);
+      setWrongFileType(true);
+    }
+  }
 
   return (
     <div className='flex w-full h-full'>
@@ -25,7 +49,40 @@ const Upload = () => {
               <p>Uploading</p>
             ) : (
               <div>
+                {videoAsset ? (
+                  <div>
 
+                  </div>
+                ) : (
+                  <label className='cursor-pointer'>
+                    <div className='flex flex-col items-center justify-center h-full'>
+                      <div className='flex flex-col items-center justify-center'>
+                        <p className='font-bold text-xl'>
+                          <FaCloudUploadAlt className='text-gray-300 text-6xl' />
+                        </p>
+                        <p className='text-xl font-semibold'>
+                          Upload Video
+                        </p>
+                      </div>
+                      <p className='text-gray-400 text-center mt-10 text-sm leading-10'>
+                        MP4 or WebM or ogg <br />
+                        720x1280 resolution or higher <br />
+                        Up to 10 minutes <br />
+                        Less than 2 GB
+                      </p>
+                      <p className='bg-[#F51997] text-center mt-8 rounded text-white text-md font-medium p-2 w-52 outline-none'>
+                        Select file
+                      </p>
+                    </div>
+                    {/* make the button clickable by adding an input tag */}
+                    <input 
+                    type="text" 
+                    name="upload-video"
+                    onChange={uploadVideo}
+                    className="w-0 h-0"
+                    />
+                  </label>
+                )}
               </div>
             )}
           </div>
